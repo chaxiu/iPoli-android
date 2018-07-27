4074bb58f68760b76b284a12442e7991ad8e015d
Polina Zhelyazkova <poly_vjk@abv.bg>
2018-07-27 18:17:19 +0300

    Layout

--- app/src/main/java/io/ipoli/android/habit/widget/HabitWidgetViewFactory.kt
+++ app/src/main/java/io/ipoli/android/habit/widget/HabitWidgetViewFactory.kt
@@ -3,8 +3,12 @@ package io.ipoli.android.habit.widget
 import android.content.Context
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
+import com.mikepenz.iconics.IconicsDrawable
 import io.ipoli.android.R
 import io.ipoli.android.common.di.BackgroundModule
+import io.ipoli.android.common.view.AndroidColor
+import io.ipoli.android.common.view.AndroidIcon
+import io.ipoli.android.common.view.normalIcon
 import io.ipoli.android.habit.data.Habit
 import io.ipoli.android.myPoliApp
 import space.traversal.kapsule.Injects
@@ -39,12 +43,33 @@ class HabitWidgetViewsFactory(private val context: Context) :
     override fun hasStableIds() = true
 
     override fun getViewAt(position: Int): RemoteViews {
+
         return items[position].let {
             RemoteViews(context.packageName, R.layout.item_widget_habit).apply {
+                setTextViewText(R.id.habitName, it.name)
+                val icon = it.icon.let { AndroidIcon.valueOf(it.name).icon }
+
+                val iconColor = AndroidColor.valueOf(it.color.name).color500
+                val iconDrawable =
+                    IconicsDrawable(context).normalIcon(icon, iconColor)
+
+                setImageViewBitmap(R.id.habitIcon, iconDrawable.toBitmap())
+
+                setProgressBar(R.id.habitProgress, 2, 1, false)
+
+
+//                val progressDrawable = drawable.getDrawable(1)
+//                progressDrawable.setColorFilter(
+//                    iconColor,
+//                    PorterDuff.Mode.SRC_ATOP
+//                )
+//
+//                this.
 
             }
         }
     }
+
 
     override fun getCount() = items.size
 
