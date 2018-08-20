d45fad30975cb513654e0a9c2278df5733d20d64
Venelin Valkov <venelin@curiousily.com>
2018-08-21 00:27:22 +0300

    Show section items

--- app/src/main/java/io/ipoli/android/quest/schedule/today/TodayViewController.kt
+++ app/src/main/java/io/ipoli/android/quest/schedule/today/TodayViewController.kt
@@ -2,16 +2,21 @@ package io.ipoli.android.quest.schedule.today
 
 import android.os.Bundle
 import android.support.design.widget.AppBarLayout
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import io.ipoli.android.MainActivity
 import io.ipoli.android.R
 import io.ipoli.android.common.redux.android.ReduxViewController
 import io.ipoli.android.common.view.AppBarStateChangeListener
 import io.ipoli.android.common.view.inflate
+import io.ipoli.android.common.view.recyclerview.MultiViewRecyclerViewAdapter
+import io.ipoli.android.common.view.recyclerview.RecyclerViewViewModel
 import io.ipoli.android.common.view.setToolbar
 import kotlinx.android.synthetic.main.controller_today.view.*
+import org.threeten.bp.LocalDate
 import space.traversal.kapsule.required
 
 class TodayViewController(args: Bundle? = null) :
@@ -57,10 +62,44 @@ class TodayViewController(args: Bundle? = null) :
             onError = { _ -> }
         )
 
+        view.todayItems.layoutManager = LinearLayoutManager(view.context)
+        view.todayItems.adapter = TodayItemAdapter()
+
         return view
     }
 
+    override fun onCreateLoadAction() = TodayAction.Load(LocalDate.now())
+
     override fun render(state: TodayViewState, view: View) {
+        (view.todayItems.adapter as TodayItemAdapter).updateAll(
+            listOf(
+                TodayItemViewModel.Section("Morning"),
+                TodayItemViewModel.Section("Afternoon"),
+                TodayItemViewModel.Section("Evening")
+            )
+        )
+    }
+
+    sealed class TodayItemViewModel(override val id: String) : RecyclerViewViewModel {
+        data class Section(val text: String) : TodayItemViewModel(text)
+    }
+
+    enum class ViewType {
+        SECTION,
+        QUEST,
+        COMPLETED_QUEST
+    }
+
+    inner class TodayItemAdapter : MultiViewRecyclerViewAdapter<TodayItemViewModel>() {
+
+        override fun onRegisterItemBinders() {
+            registerBinder<TodayItemViewModel.Section>(
+                ViewType.SECTION.ordinal,
+                R.layout.item_list_section
+            ) { vm, view, _ ->
+                (view as TextView).text = vm.text
+            }
+        }
 
     }
 
--- app/src/main/java/io/ipoli/android/quest/schedule/today/TodayViewState.kt
+++ app/src/main/java/io/ipoli/android/quest/schedule/today/TodayViewState.kt
@@ -4,9 +4,10 @@ import io.ipoli.android.common.AppState
 import io.ipoli.android.common.BaseViewStateReducer
 import io.ipoli.android.common.redux.Action
 import io.ipoli.android.common.redux.BaseViewState
+import org.threeten.bp.LocalDate
 
 sealed class TodayAction : Action {
-
+    data class Load(val today: LocalDate) : TodayAction()
 }
 
 object TodayReducer : BaseViewStateReducer<TodayViewState>() {
