46ce81145c44e5aa7a65f10c92244d5d30a51039
Venelin Valkov <venelin@curiousily.com>
2018-08-21 10:10:10 +0300

    Add toolbar styling

--- app/src/main/java/io/ipoli/android/quest/schedule/today/TodayViewController.kt
+++ app/src/main/java/io/ipoli/android/quest/schedule/today/TodayViewController.kt
@@ -1,21 +1,26 @@
 package io.ipoli.android.quest.schedule.today
 
+import android.content.res.ColorStateList
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.support.annotation.ColorRes
 import android.support.design.widget.AppBarLayout
+import android.support.v4.widget.TextViewCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.mikepenz.iconics.typeface.IIcon
 import io.ipoli.android.MainActivity
 import io.ipoli.android.R
+import io.ipoli.android.common.ViewUtils
 import io.ipoli.android.common.redux.android.ReduxViewController
-import io.ipoli.android.common.view.AppBarStateChangeListener
-import io.ipoli.android.common.view.inflate
+import io.ipoli.android.common.view.*
 import io.ipoli.android.common.view.recyclerview.MultiViewRecyclerViewAdapter
 import io.ipoli.android.common.view.recyclerview.RecyclerViewViewModel
-import io.ipoli.android.common.view.setToolbar
 import kotlinx.android.synthetic.main.controller_today.view.*
+import kotlinx.android.synthetic.main.item_agenda_quest.view.*
 import org.threeten.bp.LocalDate
 import space.traversal.kapsule.required
 
@@ -65,6 +70,8 @@ class TodayViewController(args: Bundle? = null) :
         view.todayItems.layoutManager = LinearLayoutManager(view.context)
         view.todayItems.adapter = TodayItemAdapter()
 
+        view.backdropTransparentColor.background.alpha = (255f * 0.8).toInt()
+
         return view
     }
 
@@ -74,14 +81,37 @@ class TodayViewController(args: Bundle? = null) :
         (view.todayItems.adapter as TodayItemAdapter).updateAll(
             listOf(
                 TodayItemViewModel.Section("Morning"),
+                TodayItemViewModel.QuestViewModel(
+                    id = "1234",
+                    name = "Do the laundry",
+                    tags = emptyList(),
+                    startTime = "9:30 - 10:00",
+                    color = AndroidColor.BLUE.color500,
+                    icon = AndroidIcon.WASHING_MACHINE.icon,
+                    isRepeating = false,
+                    isFromChallenge = false
+                ),
                 TodayItemViewModel.Section("Afternoon"),
                 TodayItemViewModel.Section("Evening")
             )
         )
     }
 
+    data class TagViewModel(val name: String, @ColorRes val color: Int)
+
     sealed class TodayItemViewModel(override val id: String) : RecyclerViewViewModel {
         data class Section(val text: String) : TodayItemViewModel(text)
+
+        data class QuestViewModel(
+            override val id: String,
+            val name: String,
+            val tags: List<TagViewModel>,
+            val startTime: String,
+            @ColorRes val color: Int,
+            val icon: IIcon,
+            val isRepeating: Boolean,
+            val isFromChallenge: Boolean
+        ) : TodayItemViewModel(id)
     }
 
     enum class ViewType {
@@ -93,12 +123,58 @@ class TodayViewController(args: Bundle? = null) :
     inner class TodayItemAdapter : MultiViewRecyclerViewAdapter<TodayItemViewModel>() {
 
         override fun onRegisterItemBinders() {
+
             registerBinder<TodayItemViewModel.Section>(
                 ViewType.SECTION.ordinal,
                 R.layout.item_list_section
             ) { vm, view, _ ->
                 (view as TextView).text = vm.text
             }
+
+            registerBinder<TodayItemViewModel.QuestViewModel>(
+                ViewType.QUEST.ordinal,
+                R.layout.item_agenda_quest
+            ) { vm, view, _ ->
+                view.questName.text = vm.name
+
+                view.questIcon.backgroundTintList =
+                    ColorStateList.valueOf(colorRes(vm.color))
+                view.questIcon.setImageDrawable(listItemIcon(vm.icon))
+
+                if (vm.tags.isNotEmpty()) {
+                    view.questTagName.visible()
+                    renderTag(view, vm.tags.first())
+                } else {
+                    view.questTagName.gone()
+                }
+
+                view.questStartTime.text = vm.startTime
+
+                view.questRepeatIndicator.visibility =
+                    if (vm.isRepeating) View.VISIBLE else View.GONE
+                view.questChallengeIndicator.visibility =
+                    if (vm.isFromChallenge) View.VISIBLE else View.GONE
+            }
+        }
+
+        private fun renderTag(view: View, tag: TagViewModel) {
+            view.questTagName.text = tag.name
+            TextViewCompat.setTextAppearance(
+                view.questTagName,
+                R.style.TextAppearance_AppCompat_Caption
+            )
+
+            val indicator = view.questTagName.compoundDrawablesRelative[0] as GradientDrawable
+            indicator.mutate()
+            val size = ViewUtils.dpToPx(8f, view.context).toInt()
+            indicator.setSize(size, size)
+            indicator.setColor(colorRes(tag.color))
+            view.questTagName.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                indicator,
+                null,
+                null,
+                null
+            )
         }
 
     }
